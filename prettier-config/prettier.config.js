584af35d54fd1066d53dea8480707b537e4b2747
Lyulyaev Maxim <lyulyaev.maxim@gmail.com>
2022-08-22 21:33:46 +0300

    [prettier-config][Feature] Add custom configuration of Prettier as separate package

---
+++ prettier-config/prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+}
